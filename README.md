6c826fd23a7c12cd4a3cf4b68ae55a86e3992624
BrianH <brianhwang29@gmail.com>
2021-04-29 15:50:06 +0900

    simple login

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nodejs_basic_login
 simple login 
+
+---
+..* Node.js
+..* Express
+..* mySql
+..* without sequelize
---
+++ public/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+hide(); 
+function hide(){
+    if(document.getElementById('join-btn').clicked == true){
+        document.getElementsByClassName('container').hidden = true; 
+    }
+}
