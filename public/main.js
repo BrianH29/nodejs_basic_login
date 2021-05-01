544cd34836e3baf5ba13adcf7e86c3ca98aa2d6b
brian <brianhwang29@gmail.com>
2021-05-01 10:05:44 +0900

    readme img

--- public/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-hide(); 
-function hide(){
-    if(document.getElementById('join-btn').clicked == true){
-        document.getElementsByClassName('container').hidden = true; 
-    }
-}
