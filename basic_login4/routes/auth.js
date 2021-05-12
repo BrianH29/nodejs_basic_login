1014237708a7d5d9c48d95f2c6b4e2c18f347105
BrianH <brianhwang29@gmail.com>
2021-05-12 16:09:01 +0900

    login setup

--- basic_login4/routes/auth.js
+++ basic_login4/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const User = require('../models/user'); 
-const passport = require('passport');
+const {isLoggedIn, isNotLoggedIn} = require('./authenticate');
+const passport = require('passport'); 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
@@ -11,22 +12,50 @@ router.post('/join', async (req, res, next) => {
     try{
         //check if the user is already exists
         const userExists = await User.findOne({where:{email}});
-        console.log("checking the value", check); 
         
         if(userExists){
             return res.redirect('/join?error=exists');
         }
 
         const salt = await bcrypt.genSalt(saltRounds);
-        const hash = await bcrypt.hash(req.body.password, salt); 
+        const hash = await bcrypt.hash(password, salt); 
 
         await User.create({
             email,
             nick,
             password : hash,
         })
-
+        return res.redirect('/');
     }catch(err) {
         console.error(err); 
+        return next(err); 
     }
+});
+
+router.post('/login', async(req, res, next) => {
+    passport.authenticate('local', (authError, user, info) => {
+        if(authError){
+            console.error(authError);
+            return next(authError); 
+        }
+
+        if(!user){
+            return res.redirect(`/loginError=${info.message}`);
+        }
+
+        req.login(user, (loginError) => {
+            if(loginError){
+                return next(loginError); 
+            }
+            return res.redirect('/home');
+        });
+    })(req,res,next); 
+});
+
+router.get('/logout', (req,res) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
 })
+
+module.exports = router; 
